6678c412ff6c710d1a567e7050f5b6822abc6704
Dillon Chappell <chappell.dillon@gmail.com>
2021-01-13 17:59:29 -0600

    Added more documentation as a framework.

--- Program.cs
+++ Program.cs
@@ -51,14 +51,14 @@ namespace ChappellEberle_Assign1
     // Item Class
     class Item
     {
-        readonly uint id;       // Read only
-        string name;            // Free read/write access
-        ItemType Type;          // Free read/write access; Range is [0, 12]
-        uint ilvl;              // Free read/write access; Range is [0, MAX_ILVL] 
-        uint primary;           // Free read/write access; Range is [0, MAX_PRIMARY] 
-        uint stamina;           // Free read/write access; Range is [0, MAX_STAMINA] 
-        uint requirement;       // Free read/write access; Range is [0, MAX_LEVEL] 
-        string flavor;          // Free read/write access 
+        public readonly uint id;       // Read only
+        public string name;            // Free read/write access
+        public ItemType Type;          // Free read/write access; Range is [0, 12]
+        public uint ilvl;              // Free read/write access; Range is [0, MAX_ILVL] 
+        public uint primary;           // Free read/write access; Range is [0, MAX_PRIMARY] 
+        public uint stamina;           // Free read/write access; Range is [0, MAX_STAMINA] 
+        public uint requirement;       // Free read/write access; Range is [0, MAX_LEVEL] 
+        public string flavor;          // Free read/write access 
     }
 
     // Player Class
@@ -86,6 +86,36 @@ namespace ChappellEberle_Assign1
                                  * appropriate/convenient. */
 
         List<uint> inventory;   // Will not have a corresponding Property.
+    }
+
+    // Menu Class
+    class Menu
+    {
+        // Ten options for Player
+        // ----------------------
+        // 1. Print All Players
+
+        // 2. Print All Guilds
+
+        // 3. Print All Gear
+
+        // 4. Print Gear List for Player
+
+        // 5. Leave Guild
+
+        // 6. Join Guild
+
+        // 7. Equip Gear
+
+        // 8. Unequip Gear
+
+        // 9. Award Experience
+
+        // 10. Quit (Triggered by entering "10", "q", "Q",
+        //           "quit", "Quit", "exit", or "Exit")
+
+        // 11. IComparable Testing Method (Not shown in menu,
+        //                             but accessed with "T")
     }
 
     // Equip Gear Method
